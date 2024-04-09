531df00e05d0e5130985bb37144c250ccdcc40c0
Higor Costa <higorcosta972@gmail.com>
2024-04-09 10:33:54 -0300

    feat(header.jsx): adiciona o componente Header
    Cria a estrutura do cabeçalho da página e define o estilo através do uso de css modules e
    styled-components
    
    BREAKING CHANGE: Cria o cabeçalho da página

---
+++ src/assets/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+// Estilos
+import styles from '../styles/components/Header.module.css';
+import StyledButton from '../styles/components/Button';
+
+const Header = () => {
+  return (
+    <header>
+      <div className={styles.conteinerConteudo}>
+        <div className={styles.texto}>
+          <h1>
+            Encontre os <span className={styles.textoDestaque}>melhores artigos</span> <br /> de programação em um só lugar
+          </h1>
+          <p>
+            Explore o topo da programação em um só lugar! <br /> Seu destino único
+            para dicas e tendências atuais.
+          </p>
+          <StyledButton as="a" href="#">Buscar artigos</StyledButton>
+          <StyledButton type="submit">Enviar</StyledButton>
+        </div>
+        <div className={styles.imagem}>
+          <img src="src\assets\img\image.svg" alt="Caixas de presentes" />
+        </div>
+      </div>
+    </header>
+  )
+};
+
+export default Header;
