84f96abcb1fe6cbf60cbc105d2855d2fde334db9
Higor Costa <higorcosta972@gmail.com>
2024-04-09 10:41:06 -0300

    feat(button.jsx): define estilo para os botões
    Estilo dos botões definidos com uso de styled-components

---
+++ src/assets/styles/components/Button.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  background: var(--buttonColor);
+  color: var(--white);
+  padding: 14px 32px;
+  border-radius: 4px;
+  text-transform: uppercase;
+`;
+
+export default StyledButton;
