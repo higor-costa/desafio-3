6bf6ec1b054969ee502043cecfecfced66b9275f
Higor Costa <higorcosta972@gmail.com>
2024-04-15 08:15:41 -0300

    style(/components): adiciona estilo do formulário
    Estiliza o formulário utilizando css modules e styled-components
    
    BREAKING CHANGE: Adiciona dois novos arquivos: Form.jsx e Form.module.css

---
+++ src/assets/styles/components/Form.jsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const StyledForm = styled.form`
+  width: 30rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+`;
+
+const StyledButton = styled.input`
+  background: var(--buttonColor);
+  color: var(--white);
+  border-radius: 4px;
+  padding: 10px 0;
+  text-transform: uppercase;
+  cursor: pointer;
+`;
+
+export { StyledForm, StyledButton };
