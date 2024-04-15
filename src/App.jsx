a70f394b793e70ef658df7b3b424a621717f2a37
Higor Costa <higorcosta972@gmail.com>
2024-04-15 08:28:34 -0300

    feat(app.jsx): importa footer.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 // Componentes
 import Header from './assets/components/Header';
 import Articles from './assets/components/Articles';
+import Footer from './assets/components/Footer';
 
 const App = () => {
   return (
     <>
       <Header />
       <Articles />
+      <Footer />
     </>
   )
 }
